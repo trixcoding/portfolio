c2900f7f63ef1286bae64b3972928c859b98b047
trixcoding <79534613+trixcoding@users.noreply.github.com>
2025-10-21 22:11:13 +0330

    Update page.js

--- portfolio/resume/frontend/src/app/page.js
+++ portfolio/resume/frontend/src/app/page.js
@@ -20,11 +20,11 @@ export default function Home() {
         <div className="max-w-6xl mx-auto px-5">
           <div className="glass flex items-center justify-between px-5 py-3">
             <span className="font-bold tracking-tight text-white text-m">MhmdNsr<br/>Full-Stack Wizardry 🧙</span>
-            <div className="flex gap-3 text-sm">
-              <a href="#skills" className="text-white/80 hover:text-white">مهارت‌ها</a>
-              <a href="#faq" className="text-white/80 hover:text-white">پرسش | پاسخ</a>
-                  <a href="#sample" className="text-white/80 hover:text-white">نمونه کار</a>
-            </div>
+            // <div className="flex gap-3 text-sm">
+            //   <a href="#skills" className="text-white/80 hover:text-white">مهارت‌ها</a>
+            //   <a href="#faq" className="text-white/80 hover:text-white">پرسش | پاسخ</a>
+            //       <a href="#sample" className="text-white/80 hover:text-white">نمونه کار</a>
+            // </div>
           </div>
         </div>
       </nav>
